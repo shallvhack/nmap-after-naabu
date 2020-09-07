fa7b88beb9e694690e2bd2357cfd5b8ab9feacda
Harshit Sengar <36125236+sengarharshit1@users.noreply.github.com>
2020-09-07 12:36:26 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+naabu -ports full -hL $1 -o naabu_scan.txt
+cat scan.txt | cut -d ':' -f2 | sort -u > ports.txt
+for i in `cat scan.txt | cut -d ':' -f1 |uniq`; do nmap -sS -Pn -sV -T3 -p $(sed ':a;N;$!ba;s/\n/,/g' ports.txt) $i -oN $i.txt;done
+rm ports.txt
