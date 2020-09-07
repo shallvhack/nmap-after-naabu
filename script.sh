adf7eb49c0d623e7b57847f9b6f9990d18b64d60
Harshit Sengar <36125236+sengarharshit1@users.noreply.github.com>
2020-09-07 16:53:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,7 +22,12 @@ echo "
 
 "
 
-naabu -ports full -hL $1 -o naabu_scan.txt || naabu -ports full -host $1 -o naabu_scan.txt
+naabu -ports full -hL $1 -o naabu_scan.txt || naabu -ports full -host $1 -o naabu_scan.txt || echo "
+Usage:
+./script.sh ip_list.txt
+./script.sh ip_range (e.g., ./script.sh 192.168.1.1/24)
+./script.sh single_ip (e.g., ./script.sh 192.168.43.68)
+"
 cat naabu_scan.txt | cut -d ':' -f2 | sort -u > ports.txt
 for i in `cat naabu_scan.txt | cut -d ':' -f1 |uniq`; do nmap -sS -Pn -sV -T3 -p $(sed ':a;N;$!ba;s/\n/,/g' ports.txt) $i -oN nmap_$i_scan.txt;done
 rm ports.txt
