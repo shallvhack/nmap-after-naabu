2507cc5211d55a3c9c9c7065d0f27a54dcea6844
Harshit Sengar <36125236+sengarharshit1@users.noreply.github.com>
2020-09-07 16:42:30 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ echo "
 
 "
 
-naabu -ports full -hL $1 -o naabu_scan.txt
+naabu -ports full -hL $1 -o naabu_scan.txt || naabu -ports full -host $1 -o naabu_scan.txt
 cat naabu_scan.txt | cut -d ':' -f2 | sort -u > ports.txt
 for i in `cat naabu_scan.txt | cut -d ':' -f1 |uniq`; do nmap -sS -Pn -sV -T3 -p $(sed ':a;N;$!ba;s/\n/,/g' ports.txt) $i -oN nmap_$i_scan.txt;done
 rm ports.txt
